72dc1c64a50b75092df4679411da8ba665552f27
Alberto Jose <albertojcvs@gmail.com>
2021-11-18 12:07:26 -0300

    fix: fix message when user try add to cart a game that isn't complete

--- src/js/addTocart.js
+++ src/js/addTocart.js
@@ -16,10 +16,12 @@ export function addToCart() {
   const selectedNumbers = Array.from(
     document.getElementsByClassName("number-selected")
   ).map((element) => element.textContent);
+  
   const gameSelected = getNameOfGameSelected();
   const gameData = getGame(gameSelected);
+  
   if (selectedNumbers.length < gameData["max-number"]) {
-    return alert(`Você deve selecionar ${gameData["max-number"]} números!`);
+    return alert(`Você deve selecionar mais ${gameData["max-number"] - selectedNumbers.length} ${ gameData["max-number"] - selectedNumbers.length > 1 ? 'números':'número'}!`);
   } else if (selectedNumbers.length > gameData["max-number"]) {
     return alert(`Você deve selecionar apenas ${gameData["max-number"]} números
     `);
--- src/js/getCartItemData.js
+++ src/js/getCartItemData.js
@@ -1,7 +1,6 @@
 export function getCartItemData(cartItem){
     const type  = cartItem.querySelector('span[id=gameType]').textContent;
     const numbers = cartItem.querySelector('.game-info-numbers div').textContent.split(', ');
-    console.log(type,numbers);
     return {
         type,
         numbers
